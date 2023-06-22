b491b299bf416bcc565eabe3f239d0e778331f4f
Antonio <antonio.tagua@hotmail.es>
2023-06-22 16:25:13 +0200

    make Voronoi models
    -Make Voronoi models from segmented tissue as a mask and the centroids of the cells as seeds.
    -Quantify scutoids of the same valid region as segmented cells to compare the proportion

---
+++ mainVoronoiPacking.m
@@ -0,0 +1,71 @@
+%% First pipeline to modify mistakes on S. Glands
+addpath(genpath('src'))
+addpath(genpath('lib'))
+
+clear all
+close all
+
+inPath = uigetdir('E:\Antonio\SeaStar Proyect\SeaStar_Segmentation\animalEmbryos\20200114_pos1');
+outPath = uigetdir('E:\Antonio\SeaStar Proyect\SeaStar_Segmentation\animalEmbryos\20200114_pos1');
+
+embryosFiles=dir(inPath);
+dirEmbryos = [embryosFiles.isdir];
+subDirs = embryosFiles(dirEmbryos); 
+embryosFiles = subDirs(3:end);
+
+for nEmbryos=1:length(embryosFiles)
+    segmentedEmbryosFiles = dir(strcat(embryosFiles(nEmbryos).folder,'\',embryosFiles(nEmbryos).name,'\segmentedImages\*.tif*'));
+    originalEmbryosFiles = dir(strcat(embryosFiles(nEmbryos).folder,'\',embryosFiles(nEmbryos).name,'\originalImages\*.tif*'));
+%     segmentedEmbryosFiles = dir(strcat(embryosFiles(nEmbryos).folder,'\',embryosFiles(nEmbryos).name,'\512\*.tif*'));
+%     originalEmbryosFiles = dir(strcat(embryosFiles(nEmbryos).folder,'\',embryosFiles(nEmbryos).name,'\',embryosFiles(nEmbryos).name,'.mat'));
+ embryoPath=strcat(outPath,'\',embryosFiles(nEmbryos).name);    
+    if exist(embryoPath,'file') ~=7
+        mkdir(outPath, embryosFiles(nEmbryos).name)
+    end
+
+    allGeneralInfo = cell(size(segmentedEmbryosFiles,1),1);
+    for nFiles=1:length(segmentedEmbryosFiles)
+
+        originalImagePath = originalEmbryosFiles(nFiles).folder;
+        segmentPath = segmentedEmbryosFiles(nFiles).folder;
+        imageName=originalEmbryosFiles(nFiles).name;
+        segmentName=segmentedEmbryosFiles(nFiles).name;
+        fileName=strsplit(segmentName,'_itkws');
+        fileName=strsplit(fileName{1},'.tif');
+        
+        %extract scale and resize tissue.
+        [~,~,~,labelledImage,z_Scale,pixel_Scale,originalImage] = seaStarOnlyExtractValidCells(originalImagePath,segmentPath,imageName,segmentName);        
+        
+        %make Voronoi models
+        [voronoiCyst]=makeVoronoiModels(originalImage,labelledImage,embryoPath,fileName{1});
+        [basalLayer,apicalLayer,lateralLayer,voronoiCyst]=resizeTissue(embryoPath,fileName{1},voronoiCyst,1,0);
+        voronoiCystResized=imresize3(voronoiCyst,[size(originalImage,1:2) size(originalImage,3)*z_Scale],'nearest');
+        
+        %select valid cells
+        [numberTotalCells,validCells,numberValidCells]=filterValidRegion(voronoiCystResized,pixel_Scale);
+        
+        %Quantify scutoids
+        dilatedVx=2;
+        contactThreshold=0.5;
+        [scutoids_cells,validScutoids_cells,surfaceRatio3D]=calculateScutoidsAndSR(voronoiCyst,apicalLayer,basalLayer,lateralLayer,embryoPath,fileName{1},dilatedVx,contactThreshold,validCells);
+        generalInfo= cell2table([{fileName(1)}, {surfaceRatio3D}, {numberValidCells},{numberTotalCells},{mean(scutoids_cells)},{mean(validScutoids_cells)}],'VariableNames', {'ID_Tissue', 'SurfaceRatio3D_areas', 'NCells_valid','NCells_total','Scutoids','valid_Scutoids'});
+        allGeneralInfo{nFiles} = generalInfo;
+
+        %export valid region and scutoids
+        exportValidRegion(embryoPath,fileName{1},voronoiCyst,originalImage,validScutoids_cells,validCells)
+        
+        
+    end
+    
+    summarizeAllTissuesProperties(allGeneralInfo,[],[],[],embryoPath,embryosFiles(nEmbryos).name,0);
+    
+end
+
+
+
+
+
+
+
+
+
---
+++ makeVoronoiModels.m
@@ -0,0 +1,38 @@
+function [voronoiCyst]=makeVoronoiModels(originalImage,labelledImage,outPath,fileName)
+
+if exist(strcat(outPath,'\','voronoi_',fileName,'.tif'),'file')~=2
+
+labelledImage=imresize3(labelledImage,(size(labelledImage)/4),'nearest');
+
+%% specify mask
+cellSpace=labelledImage>1;
+
+%% calculate centroids
+centroids=regionprops3(labelledImage,'Centroid');
+centroids=centroids.Centroid;
+[indexEmpty,~]=find(isnan(centroids(:,3)));
+centroids(indexEmpty,:)=[];
+
+
+%% make seeds
+seedMatrix = zeros(size(cellSpace));
+
+for nSeed=1:length(centroids)
+    y=round(centroids(nSeed,1));
+    x=round(centroids(nSeed,2));
+    z=round(centroids(nSeed,3));
+    seedMatrix(x,y,z)=1;  
+end
+
+
+seeds = bwlabeln(seedMatrix);
+
+%% make Voronoi
+voronoiCyst = VoronoizateCells(cellSpace,seeds);
+
+resizedVoronoiCyst=imresize3(voronoiCyst,size(originalImage),'nearest');
+writeStackTif(uint16(resizedVoronoiCyst), strcat(outPath,'\','voronoi_',fileName,'.tif'));
+else
+voronoiCyst=readStackTif(strcat(outPath,'\','voronoi_',fileName,'.tif'));
+voronoiCyst=imresize3(double(voronoiCyst),(size(labelledImage)/4),'nearest');
+end
