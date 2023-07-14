155f56ea1d27ecacd674db42eb7a7d477022a610
jansanrom <jesusangel.asr@gmail.com>
2023-07-14 11:01:25 +0200

    Update readStackTif.m

--- utils/readStackTif.m
+++ utils/readStackTif.m
@@ -1,5 +1,15 @@
 function [loadedImage, infoImage] = readStackTif(fileName)
 
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % readStackTif
+    % Function to read tif file stack images.
+    % INPUTS:
+    % fileName: full path (including fileName and '.tif')
+    % OUTPUTS:
+    % loadedImage: stack image
+    % infoImage: Image metadata
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
     infoImage = imfinfo(fileName);
     loadedImage = uint16(zeros(infoImage(1).Height,infoImage(1).Width,size(infoImage,1)));
     for nZ = 1:size(infoImage,1)
