fc1307695bd5ae001a9acb4ad164d1d18fc16ebf
Antonio <antonio.tagua@hotmail.es>
2024-01-11 10:03:25 +0100

    Update exportValidRegion.m
    export resized images and images with only scutoids labels

--- featuresExtraction/exportValidRegion.m
+++ featuresExtraction/exportValidRegion.m
@@ -1,29 +1,27 @@
 function exportValidRegion(path2save,fileName,labelledImage,originalImage,valid_scutoids,validCells)
 
-
-
-noValidCells=setdiff(labelledImage,validCells);
-noValidCells(noValidCells==0)=[];
+    noValidCells=setdiff(labelledImage,validCells);
+    noValidCells(noValidCells==0)=[];
     if ismember(1,validCells)
         labelledImage(labelledImage==1)=max(max(max(labelledImage)))+1;
         [index]=find(validCells==1);
         validCells(index)=max(max(max(labelledImage)))+1;
     end
-for nCell=1:length(noValidCells)
-    labelledImage(labelledImage==noValidCells(nCell))=1;   
-end
-
-resizedLabelledImage=imresize3(labelledImage,size(originalImage),'nearest');
-writeStackTif(uint16(resizedLabelledImage), strcat(path2save,'\','resized_',fileName,'.tif'));
-
-for nCell=1:length(valid_scutoids)
-    if valid_scutoids(nCell)==0
-        resizedLabelledImage(resizedLabelledImage==validCells(nCell))=1;
+    for nCell=1:length(noValidCells)
+        labelledImage(labelledImage==noValidCells(nCell))=1;
     end
-end
-if ~all(valid_scutoids)
-writeStackTif(uint16(resizedLabelledImage), strcat(path2save,'\','validScutoids_',fileName,'.tif'));
-end
+    
+    resizedLabelledImage=imresize3(labelledImage,size(originalImage),'nearest');
+    writeStackTif(uint16(resizedLabelledImage), strcat(path2save,'\','resized_',fileName,'.tif'));
+    
+    for nCell=1:length(valid_scutoids)
+        if valid_scutoids(nCell)==0
+            resizedLabelledImage(resizedLabelledImage==validCells(nCell))=1;
+        end
+    end
+    if ~all(valid_scutoids)
+        writeStackTif(uint16(resizedLabelledImage), strcat(path2save,'\','validScutoids_',fileName,'.tif'));
+    end    
 
 end
 
