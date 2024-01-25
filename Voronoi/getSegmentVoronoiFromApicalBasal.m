a22129b01a44d879137201bbb056a283334ec9f3
Antonio <antonio.tagua@hotmail.es>
2024-01-25 12:00:15 +0100

    updating establishment of inner and outer layers

--- Voronoi/getSegmentVoronoiFromApicalBasal.m
+++ Voronoi/getSegmentVoronoiFromApicalBasal.m
@@ -1,8 +1,8 @@
 function [voronoiEmbryo]=getSegmentVoronoiFromApicalBasal(labelledImage,outPath,fileName)
-[basalLayer,apicalLayer,~,labelledImage]=getInnerOuterLateralFromEmbryos('','',labelledImage,1,0);
+[innerLayer,outerLayer,~,labelledImage]=getInnerOuterLateralFromEmbryos('','',labelledImage,1,0);
 labelledImage=imresize3(labelledImage,(size(labelledImage)/4),'nearest');
-apicalLayer=imresize3(apicalLayer,(size(apicalLayer)/4),'nearest');
-basalLayer=imresize3(basalLayer,(size(basalLayer)/4),'nearest');
+outerLayer=imresize3(outerLayer,(size(outerLayer)/4),'nearest');
+innerLayer=imresize3(innerLayer,(size(innerLayer)/4),'nearest');
 
 %% specify mask
 cellSpace=labelledImage>1;
@@ -33,52 +33,52 @@ segmentSeedMatrix = zeros(size(labelledImage));
 
 for cellIx = 2:length(uniqueLabels)
     cellId = uniqueLabels(cellIx);
-    auxApicalLayer = apicalLayer==cellId;
-    auxBasalLayer = basalLayer==cellId;
+    auxOuterLayer = outerLayer==cellId;
+    auxInnerLayer = innerLayer==cellId;
     
     centroid = regionprops3(labelledImage==cellId, 'centroid');
     centroid = centroid.Centroid;
     centroid = mean(centroid,  1);
-    apicalCentroid = regionprops3(auxApicalLayer, 'centroid');
-    apicalCentroid = mean(apicalCentroid.Centroid,1);
-    basalCentroid = regionprops3(auxBasalLayer, 'centroid');
-    basalCentroid = mean(basalCentroid.Centroid,1);
+    outerCentroid = regionprops3(auxOuterLayer, 'centroid');
+    outerCentroid = mean(outerCentroid.Centroid,1);
+    innerCentroid = regionprops3(auxInnerLayer, 'centroid');
+    innerCentroid = mean(innerCentroid.Centroid,1);
     
-    if isempty(basalCentroid)
+    if isempty(innerCentroid)
         try
-        basalCentroid=centroid+(centroid-apicalCentroid);
-        if basalCentroid(3)> size(labelledImage,3)
-            basalCentroid(3)=size(labelledImage,3);
-        elseif basalCentroid(2)> size(labelledImage,1)
-            basalCentroid(2)=size(labelledImage,1);
-        elseif basalCentroid(1)> size(labelledImage,2)
-            basalCentroid(1)=size(labelledImage,2);
-        elseif basalCentroid(2)<0
-            basalCentroid(2)=1;
-        elseif basalCentroid(1)<0
-            basalCentroid(1)=1;
+        innerCentroid=centroid+(centroid-outerCentroid);
+        if innerCentroid(3)> size(labelledImage,3)
+            innerCentroid(3)=size(labelledImage,3);
+        elseif innerCentroid(2)> size(labelledImage,1)
+            innerCentroid(2)=size(labelledImage,1);
+        elseif innerCentroid(1)> size(labelledImage,2)
+            innerCentroid(1)=size(labelledImage,2);
+        elseif innerCentroid(2)<0
+            innerCentroid(2)=1;
+        elseif innerCentroid(1)<0
+            innerCentroid(1)=1;
         end
         catch
             continue
         end
-    elseif isempty(apicalCentroid)
-        apicalCentroid=centroid+(centroid-basalCentroid);
-        if apicalCentroid(3)> size(labelledImage,3)
-            apicalCentroid(3)=size(labelledImage,3);
-        elseif apicalCentroid(2)> size(labelledImage,1)
-            apicalCentroid(2)=size(labelledImage,1);
-        elseif apicalCentroid(1)> size(labelledImage,2)
-            apicalCentroid(1)=size(labelledImage,2);
-        elseif apicalCentroid(2)<0
-            apicalCentroid(2)=1;
-        elseif apicalCentroid(1)<0
-            apicalCentroid(1)=1;
+    elseif isempty(outerCentroid)
+        outerCentroid=centroid+(centroid-innerCentroid);
+        if outerCentroid(3)> size(labelledImage,3)
+            outerCentroid(3)=size(labelledImage,3);
+        elseif outerCentroid(2)> size(labelledImage,1)
+            outerCentroid(2)=size(labelledImage,1);
+        elseif outerCentroid(1)> size(labelledImage,2)
+            outerCentroid(1)=size(labelledImage,2);
+        elseif outerCentroid(2)<0
+            outerCentroid(2)=1;
+        elseif outerCentroid(1)<0
+            outerCentroid(1)=1;
         end
     end
 
-    ypoints = [round(linspace(apicalCentroid(1), centroid(1), 5)), round(linspace(centroid(1), basalCentroid(1), 5));];
-    xpoints = [round(linspace(apicalCentroid(2), centroid(2), 5)), round(linspace(centroid(2), basalCentroid(2), 5));];
-    zpoints = [round(linspace(apicalCentroid(3), centroid(3), 5)), round(linspace(centroid(3), basalCentroid(3), 5));];
+    ypoints = [round(linspace(outerCentroid(1), centroid(1), 5)), round(linspace(centroid(1), innerCentroid(1), 5));];
+    xpoints = [round(linspace(outerCentroid(2), centroid(2), 5)), round(linspace(centroid(2), innerCentroid(2), 5));];
+    zpoints = [round(linspace(outerCentroid(3), centroid(3), 5)), round(linspace(centroid(3), innerCentroid(3), 5));];
     
     for ix = linspace(1,10,10)
         segmentSeedMatrix(xpoints(ix),ypoints(ix),zpoints(ix))=cellId;
